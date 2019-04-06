c30d587939f68d3fc76b1e4707ed564106979d7b
ben <51529210@qq.com>
2019-04-06 16:23:17 +0800

    u

--- uuid.go
+++ uuid.go
@@ -1,89 +1,243 @@
 package uuid
 
 import (
+	"crypto/md5"
+	"crypto/sha1"
 	"math/rand"
 	"net"
-	"sync"
+	"os"
+	"reflect"
 	"time"
+	"unsafe"
 )
 
-const (
-	uuidLength = 36
-)
+const uuidLength = 36
 
 var (
-	uuidV1Timestamp = uint64(time.Now().UnixNano())
-	uuidV1Clock     = uint16(rand.Int())
-	uuidV1Mux       sync.RWMutex
-	uuidV1Node      [6]byte
-	hexTable        = []byte("0123456789ABCDEF")
+	hexTable   = []byte("0123456789ABCDEF")
+	uuidNode   [6]byte
+	uuidClock  uint16
+	uuidV2GID  uint32
+	uuidV2UID  uint32
+	uuidV4Rand *rand.Rand
 )
 
 func init() {
-	initV1Node()
-}
-
-// MAC address default
-func initV1Node() {
+	// init node with MAC address
 	a, e := net.Interfaces()
 	if nil != e {
 		panic(e)
 	}
 	for i := 0; i < len(a); i++ {
 		if len(a[i].HardwareAddr) >= 6 {
-			copy(uuidV1Node[0:], a[i].HardwareAddr)
+			copy(uuidNode[0:], a[i].HardwareAddr)
 			break
 		}
 	}
+	// init v2 gid and uid
+	uuidV2GID = uint32(os.Getgid())
+	uuidV2UID = uint32(os.Getuid())
+	// init v4 random
+	uuidV4Rand = rand.New(rand.NewSource(time.Now().UTC().UnixNano()))
 }
 
 // set node
-func SetV1Node(node [6]byte) {
-	copy(uuidV1Node[0:], node[0:])
+func SetNode(node [6]byte) {
+	copy(uuidNode[0:], node[0:])
 }
 
-func hex(b []byte, c byte) {
-	b[0] = hexTable[c>>4]
-	b[1] = hexTable[c&0x0f]
+// set posix gid
+func SetV2GID(id uint32) {
+	uuidV2GID = id
 }
 
-// rfc4122,timestamp
+// set posix uid
+func SetV2UID(id uint32) {
+	uuidV2UID = id
+}
+
+func put32(b []byte, n uint32) {
+	b[0] = uint8(n >> 24)
+	b[2] = uint8(n >> 16)
+	b[4] = uint8(n >> 8)
+	b[6] = uint8(n)
+}
+
+func put16(b []byte, n uint16) {
+	b[0] = uint8(n >> 8)
+	b[2] = uint8(n)
+}
+
+func putBytes(b1, b2 []byte) {
+	j := 0
+	for i := 0; i < len(b2); i++ {
+		b1[j] = b2[i]
+		j += 2
+	}
+}
+
+func hex(b []byte) {
+	i, j := 0, 0
+	for ; i < 8; i += 2 {
+		j = i + 1
+		b[j] = hexTable[b[i]&0x0f]
+		b[i] = hexTable[b[i]>>4]
+	}
+	b[8] = '-'
+	i = 9
+	for ; i < 13; i += 2 {
+		j = i + 1
+		b[j] = hexTable[b[i]&0x0f]
+		b[i] = hexTable[b[i]>>4]
+	}
+	b[13] = '-'
+	i = 14
+	for ; i < 18; i += 2 {
+		j = i + 1
+		b[j] = hexTable[b[i]&0x0f]
+		b[i] = hexTable[b[i]>>4]
+	}
+	b[18] = '-'
+	i = 19
+	for ; i < 23; i += 2 {
+		j = i + 1
+		b[j] = hexTable[b[i]&0x0f]
+		b[i] = hexTable[b[i]>>4]
+	}
+	b[23] = '-'
+	i = 24
+	for ; i < uuidLength; i += 2 {
+		j = i + 1
+		b[j] = hexTable[b[i]&0x0f]
+		b[i] = hexTable[b[i]>>4]
+	}
+}
+
+func putHash(b1, b2 []byte) {
+	putBytes(b1[0:8], b2[:4])
+	b1[8] = '-'
+	putBytes(b1[9:13], b2[4:6])
+	b1[13] = '-'
+	putBytes(b1[14:18], b2[6:8])
+	b1[18] = '-'
+	putBytes(b1[18:23], b2[8:10])
+	b1[23] = '-'
+	putBytes(b1[24:uuidLength], b2[10:16])
+}
+
+func unsafeBytesFromString(s *string) []byte {
+	ss := (*reflect.StringHeader)(unsafe.Pointer(s))
+	bb := reflect.SliceHeader{
+		Data: ss.Data,
+		Len:  ss.Len,
+		Cap:  ss.Len,
+	}
+	return *(*[]byte)(unsafe.Pointer(&bb))
+}
+
+// timestamp
 func V1() string {
-	var uuid [uuidLength]byte
-	var clock uint16
+	var buf [uuidLength]byte
 	// timestamp
-	ts := uint64(time.Now().UnixNano())
-	uuidV1Mux.Lock()
-	if ts <= uuidV1Timestamp {
-		uuidV1Clock ++
-	} else {
-		uuidV1Timestamp = ts
-	}
-	clock = uuidV1Clock
-	uuidV1Mux.Unlock()
+	ts := uint64(time.Now().UTC().UnixNano())
 	// time low
-	hex(uuid[0:], uint8(ts))
-	hex(uuid[2:], uint8(ts>>8))
-	hex(uuid[4:], uint8(ts>>16))
-	hex(uuid[6:], uint8(ts>>24))
-	uuid[8] = '-'
+	put32(buf[0:], uint32(ts))
 	// time mid
-	hex(uuid[9:], uint8(ts>>32))
-	hex(uuid[11:], uint8(ts>>40))
-	uuid[13] = '-'
+	put16(buf[9:], uint16(ts>>32))
 	// time high and version
-	hex(uuid[14:], uint8(ts>>48))
-	hex(uuid[16:], uint8((uint8(ts>>56)&0x0f)|0x1f))
-	uuid[18] = '-'
-	// clock and variant
-	hex(uuid[19:], (uint8(clock>>8)&0x3f)|0x80)
-	hex(uuid[21:], uint8(clock))
-	uuid[23] = '-'
+	put16(buf[14:], uint16(ts>>48))
+	// version
+	buf[16] = (buf[16] & 0x0f) | 0x1f
+	// clock
+	uuidClock++
+	put16(buf[19:], uuidClock)
+	// variant
+	buf[19] = buf[19]&0x1f | 0x80
 	// node
-	i := 24
-	for j := 0; j < len(uuidV1Node); j++ {
-		hex(uuid[i:], uuidV1Node[j])
-		i += 2
-	}
-	return string(uuid[0:])
+	putBytes(buf[24:], uuidNode[0:])
+	// hex and return
+	hex(buf[0:])
+	return string(buf[0:])
+}
+
+// DEC
+func V2(id uint32) string {
+	var buf [uuidLength]byte
+	// timestamp
+	ts := uint64(time.Now().UTC().UnixNano())
+	put32(buf[0:], id)
+	// time low
+	put32(buf[0:], uint32(ts))
+	// time mbuf
+	put16(buf[9:], uint16(ts>>32))
+	// time high and version
+	put16(buf[14:], uint16(ts>>48))
+	// version
+	buf[16] = (buf[16] & 0x0f) | 0x2f
+	// clock
+	uuidClock++
+	put16(buf[19:], uuidClock)
+	// variant
+	buf[19] = buf[19]&0x1f | 0x80
+	// node
+	putBytes(buf[24:], uuidNode[0:])
+	// hex and return
+	hex(buf[0:])
+	return string(buf[0:])
+}
+
+func V2Gid() string {
+	return V2(uuidV2GID)
+}
+
+func V2Uid() string {
+	return V2(uuidV2UID)
+}
+
+// name md5
+func V3(uuid, name string) string {
+	h := md5.New()
+	h.Write(unsafeBytesFromString(&uuid))
+	h.Write(unsafeBytesFromString(&name))
+	var buf [uuidLength]byte
+	putHash(buf[0:], h.Sum(nil))
+	// version
+	buf[16] = (buf[16] & 0x0f) | 0x3f
+	// variant
+	buf[19] = buf[19]&0x1f | 0x80
+	hex(buf[0:])
+	return string(buf[0:])
+}
+
+// random
+func V4() string {
+	var buf [uuidLength]byte
+	put32(buf[0:], uuidV4Rand.Uint32())
+	put16(buf[9:], uint16(uuidV4Rand.Int31()))
+	put16(buf[14:], uint16(uuidV4Rand.Int31()))
+	// version
+	buf[16] = (buf[16] & 0x0f) | 0x4f
+	put32(buf[19:], uuidV4Rand.Uint32())
+	// variant
+	buf[19] = buf[19]&0x1f | 0x80
+	put32(buf[24:], uuidV4Rand.Uint32())
+	put16(buf[32:], uint16(uuidV4Rand.Int31()))
+	// hex and return
+	hex(buf[0:])
+	return string(buf[0:])
+}
+
+// name sha-1
+func V5(uuid, name string) string {
+	h := sha1.New()
+	h.Write(unsafeBytesFromString(&uuid))
+	h.Write(unsafeBytesFromString(&name))
+	var buf [uuidLength]byte
+	putHash(buf[0:], h.Sum(nil))
+	// version
+	buf[16] = (buf[16] & 0x0f) | 0x5f
+	// variant
+	buf[19] = buf[19]&0x1f | 0x80
+	hex(buf[0:])
+	return string(buf[0:])
 }
--- uuid_test.go
+++ uuid_test.go
@@ -1,14 +1,31 @@
 package uuid
 
 import (
+	"math/rand"
 	"testing"
 	satori_uuid "github.com/satori/go.uuid"
 )
 
 func TestV1(t *testing.T) {
-	for i := 0; i < 10; i++ {
-		t.Log(V1())
-	}
+	t.Log(V1())
+}
+
+func TestV2(t *testing.T) {
+	t.Log(V2Gid())
+	t.Log(V2Uid())
+	t.Log(V2(111))
+}
+
+func TestV3(t *testing.T) {
+	t.Log(V3(V1(), "md5"))
+}
+
+func TestV4(t *testing.T) {
+	t.Log(V4())
+}
+
+func TestV5(t *testing.T) {
+	t.Log(V5(V2(222), "sha1"))
 }
 
 func BenchmarkV1_My(b *testing.B) {
@@ -23,6 +40,74 @@ func BenchmarkV1_Satori(b *testing.B) {
 		if nil != e {
 			b.Fatal(e)
 		}
+		uuid.String()
+	}
+}
+
+func BenchmarkV2_My(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		V2(uint32(i))
+	}
+}
+
+func BenchmarkV2_Satori(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		uuid, e := satori_uuid.NewV2(byte(i % 2))
+		if nil != e {
+			b.Fatal(e)
+		}
+		uuid.String()
+	}
+}
+
+func BenchmarkV3_My(b *testing.B) {
+	id := V1()
+	for i := 0; i < b.N; i++ {
+		V3(id, "md5")
+	}
+}
+
+func BenchmarkV3_Satori(b *testing.B) {
+	id, e := satori_uuid.NewV1()
+	if nil != e {
+		b.Fatal(e)
+	}
+	for i := 0; i < b.N; i++ {
+		uuid := satori_uuid.NewV3(id, "md5")
+		uuid.String()
+	}
+}
+
+func BenchmarkV4_My(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		V4()
+	}
+}
+
+func BenchmarkV4_Satori(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		uuid, e := satori_uuid.NewV4()
+		if nil != e {
+			b.Fatal(e)
+		}
+		uuid.String()
+	}
+}
+
+func BenchmarkV5_My(b *testing.B) {
+	id := V2(rand.Uint32())
+	for i := 0; i < b.N; i++ {
+		V5(id, "namespace-sha1")
+	}
+}
+
+func BenchmarkV5_Satori(b *testing.B) {
+	id, e := satori_uuid.NewV2(0)
+	if nil != e {
+		b.Fatal(e)
+	}
+	for i := 0; i < b.N; i++ {
+		uuid := satori_uuid.NewV5(id, "namespace-sha1")
 		uuid.String()
 	}
 }
